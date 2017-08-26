5eebefdc1461b50144ba739f0e3857ed1f2edd8a
Ryan Seddon <seddon.ryan@gmail.com>
2017-08-26 15:16:10 +1000

    Create jpeg poster image using ffmeg instead revert log method extraction for now.

--- frames.go
+++ frames.go
@@ -4,8 +4,6 @@ import (
 	"crypto/md5"
 	"encoding/hex"
 	"fmt"
-	"image/jpeg"
-	"image/png"
 	"io"
 	"log"
 	"net/http"
@@ -16,23 +14,22 @@ import (
 
 func main() {
 	var wg sync.WaitGroup
-	frames := make([]int, 6)
+	frames := make([]int, 24*6)
 	now := time.Now()
-	// Change the date to midday AEST in UTC time
+	// Change the date to midnight AEST time
 	t := time.Date(now.Year(), now.Month(), now.Day(), 2, 00, 00, 0, time.UTC)
 	first := t.Add(-24 * time.Hour)
-	setupLogs()
 
 	for i, _ := range frames {
 		wg.Add(1)
-		go getImage(framePath(first), i, &wg, first)
+		go getImage(pathfor(first), i, &wg)
 		first = first.Add(time.Duration(10) * time.Minute)
 	}
 
 	wg.Wait()
 }
 
-func framePath(time time.Time) string {
+func pathfor(time time.Time) string {
 	url := "http://himawari8-dl.nict.go.jp/himawari8/img/D531106/1d/550"
 	// Himawari8 only has images saved in 10 minute intervals, modulo to the rescue
 	minute := time.Minute() - time.Minute()%10
@@ -40,43 +37,36 @@ func framePath(time time.Time) string {
 	return fmt.Sprintf("%s/%d/%02d/%02d/%02d%02d00_0_0.png", url, time.Year(), time.Month(), time.Day(), time.Hour(), minute)
 }
 
-func getImage(url string, name int, wg *sync.WaitGroup, date time.Time) {
+func getImage(url string, name int, wg *sync.WaitGroup) {
 	defer wg.Done()
 	response, e := http.Get(url)
 	if e != nil {
 		log.Fatal(e)
 	}
 
-	log.Printf("status: %s | frame: %s", response.StatusCode, url)
-
 	defer response.Body.Close()
 
+	// No image image doesn't seem to exist anymore instead it'll 404 on the url
 	if response.StatusCode == 200 {
+		//log url requested for later debugging
+		logs, err := os.OpenFile("logs.txt", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer logs.Close()
+		log.SetOutput(logs)
+		log.Println(url)
+
 		//open a file for writing
 		file, err := os.Create(fmt.Sprintf("%04d.png", name))
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		if name == 0 {
-			img, err1 := png.Decode(response.Body)
-			if err1 != nil {
-				log.Fatal(err1)
-			}
-
-			out, err2 := os.Create(fmt.Sprintf("%d%02d%02d.jpg", date.Year(), date.Month(), date.Day()))
-			if err2 != nil {
-				log.Fatal(err2)
-			}
-
-			err3 := jpeg.Encode(out, img, &jpeg.Options{jpeg.DefaultQuality})
-			if err3 != nil {
-				log.Fatal(err3)
-			}
-		}
 
 		// Generating md5 hashes for frames
 		hash := md5.New()
+		errorHash := "b697574875d3b8eb5dd80e9b2bc9c749"
 		var writers []io.Writer
 		writers = append(writers, hash, file)
 
@@ -91,32 +81,9 @@ func getImage(url string, name int, wg *sync.WaitGroup, date time.Time) {
 
 		// If an image matches the error file hash remove it
 		// TODO: Can I abort the file write instead?
-		errorHash := "b697574875d3b8eb5dd80e9b2bc9c749"
 		imgHash := hex.EncodeToString(hash.Sum(nil))
-
 		if imgHash == errorHash {
 			os.Remove(fmt.Sprintf("%04d.png", name))
 		}
 	}
-}
-
-//func compareHashes(hash *hash.Hash, name int) {
-//// If an image matches the error file hash remove it
-//// TODO: Can I abort the file write instead?
-//errorHash := "b697574875d3b8eb5dd80e9b2bc9c749"
-//imgHash := hex.EncodeToString(hash.Sum(nil))
-
-//if imgHash == errorHash {
-//os.Remove(fmt.Sprintf("%04d.jpg", name))
-//}
-//}
-
-func setupLogs() {
-	//log url requested for later debugging
-	logs, err := os.OpenFile("logs.txt", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer logs.Close()
-	log.SetOutput(logs)
 }
