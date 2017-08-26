267656f5e291060ce94eb6372c057cc43e254cc2
Ryan Seddon <seddon.ryan@gmail.com>
2017-08-26 15:19:32 +1000

    Update cronjob script

--- create_video.sh
+++ create_video.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
+source ~/.bash_profile
+FILENAME=`date --date="1 day ago" +%Y%m%d`
+VIDEONAME=$FILENAME.mp4
+cd ~/earthin24
 ./video
-ffmpeg -framerate 10 -i %d.png -s:v 1280x720 -c:v libx264 -profile:v high -crf 20 -pix_fmt yuv420p `date -v-1d +%Y%m%d`.mp4
-rm `find . -name '*.png' ! -name 'error.png'`
+ffmpeg -framerate 10 -pattern_type glob -i '*.png' -s:v 1280x720 -c:v libx264 -profile:v high -crf 20 -pix_fmt yuv420p ~/earthin24/$VIDEONAME
+# extract first frame as jpg for instagram
+ffmpeg -i $VIDEONAME -vf "select=eq(n\,0)" -q:v 3 -s:v 720x720 $FILENAME.jpg
+./tweet $VIDEONAME
+node instagram.js $FILENAME
+rm ~/earthin24/*.png
